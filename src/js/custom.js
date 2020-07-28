8de3465e7f331d769d267707c4af8d6667d1f225
Dastan Akhmetov <dastan.mvp97@gmail.com>
2020-07-28 19:10:08 +0600

    checkpoint

--- src/js/custom.js
+++ src/js/custom.js
@@ -28,43 +28,46 @@ function scrollFunc(){
   let element = document.getElementById('section2'),
       scrollVal = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
   if(element.offsetTop - scrollVal < 180){
+    let makeVisual = document.getElementById()
     let animation = anime.timeline({loop: false})
         .add({
-          targets: '.ml5 .line',
-          opacity: [0.5,1],
-          scaleX: [0, 1],
-          easing: "easeInOutExpo",
-          duration: 700
-        })
-        .add({
-          targets: '.ml5 .line',
-          duration: 600,
-          easing: "easeOutExpo",
-          translateY: (el, i) => (-val1 + val2*2*i) + "em"
-        })
-        .add({
-          targets: '.ml5 .ampersand',
-          opacity: [0,1],
-          scaleY: [0.5, 1],
-          easing: "easeOutExpo",
-          duration: 600,
-          offset: '-=600'
-        })
-        .add({
-          targets: '.ml5 .letters-left',
-          opacity: [0,1],
-          translateX: ["0.5em", 0],
-          easing: "easeOutExpo",
-          duration: 600,
-          offset: '-=300'
-        })
-        .add({
-          targets: '.ml5 .letters-right',
-          opacity: [0,1],
-          translateX: ["-0.5em", 0],
-          easing: "easeOutExpo",
-          duration: 600,
-          offset: '-=600'
+            targets: '.ml5 .line',
+            opacity: [0.5,1],
+            scaleX: [0, 1],
+            easing: "easeInOutExpo",
+            duration: 700
+        }).add({
+            targets: '.ml5 .line',
+            duration: 600,
+            easing: "easeOutExpo",
+            translateY: (el, i) => (-val1 + val2*2*i) + "em"
+        }).add({
+            targets: '.ml5 .ampersand',
+            opacity: [0,1],
+            scaleY: [0.5, 1],
+            easing: "easeOutExpo",
+            duration: 600,
+            offset: '-=600'
+        }).add({
+            targets: '.ml5 .letters-left',
+            opacity: [0,1],
+            translateX: ["0.5em", 0],
+            easing: "easeOutExpo",
+            duration: 600,
+            offset: '-=300'
+        }).add({
+            targets: '.ml5 .letters-right',
+            opacity: [0,1],
+            translateX: ["-0.5em", 0],
+            easing: "easeOutExpo",
+            duration: 600,
+            offset: '-=600'
+        }).add({
+            targets: '.ml5',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
         });
     window.onscroll = animation.play
   }
