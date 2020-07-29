f118dc6ebb9d1d2e935e9ab39f38180263733b79
Dastan Akhmetov <dastan.mvp97@gmail.com>
2020-07-29 15:18:03 +0600

    added png to makeVisual for snap effect

--- src/js/custom.js
+++ src/js/custom.js
@@ -21,55 +21,77 @@ switch (screenWidth) {
 
 
 window.onscroll = function(){
-  scrollFunc()
+    scrollFunc()
 }
 
 function scrollFunc(){
-  let element = document.getElementById('section2'),
-      scrollVal = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-  if(element.offsetTop - scrollVal < 180){
-    let makeVisual = document.getElementById()
-    let animation = anime.timeline({loop: false})
-        .add({
-            targets: '.ml5 .line',
-            opacity: [0.5,1],
-            scaleX: [0, 1],
-            easing: "easeInOutExpo",
-            duration: 700
-        }).add({
-            targets: '.ml5 .line',
-            duration: 600,
-            easing: "easeOutExpo",
-            translateY: (el, i) => (-val1 + val2*2*i) + "em"
-        }).add({
-            targets: '.ml5 .ampersand',
-            opacity: [0,1],
-            scaleY: [0.5, 1],
-            easing: "easeOutExpo",
-            duration: 600,
-            offset: '-=600'
-        }).add({
-            targets: '.ml5 .letters-left',
-            opacity: [0,1],
-            translateX: ["0.5em", 0],
-            easing: "easeOutExpo",
-            duration: 600,
-            offset: '-=300'
-        }).add({
-            targets: '.ml5 .letters-right',
-            opacity: [0,1],
-            translateX: ["-0.5em", 0],
-            easing: "easeOutExpo",
-            duration: 600,
-            offset: '-=600'
-        }).add({
-            targets: '.ml5',
-            opacity: 0,
-            duration: 1000,
-            easing: "easeOutExpo",
-            delay: 1000
-        });
-    window.onscroll = animation.play
-  }
+    let element = document.getElementById('section2'),
+        scrollVal = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+    if(element.offsetTop - scrollVal < 180){
+        let makeVisual = document.getElementById('makeVisual'),
+            animation = anime.timeline({loop: false}),
+            mVclasses = ['animate__animated','animate__fadeIn']
+
+        if(makeVisual){
+            makeVisual.style.visibility = 'visible'
+            makeVisual.style.setProperty('--animate-duration', '5s');
+            makeVisual.classList.add(...mVclasses)
+        }
+        if(window.screen.width > 1200){
+            let visualContainer = document.getElementById('visualContainer'),
+                img = document.createElement("img")
+            img.setAttribute("src","./assets/makeVisual.png")
+            img.setAttribute("class","makeVisualImg")
+            if(visualContainer.childElementCount === 1){
+                setTimeout(()=> {
+                    if(makeVisual) {
+                        let oldChild = document.getElementById('makeVisual')
+                        visualContainer.removeChild(oldChild)
+                        visualContainer.appendChild(img)
+                    }
+                }, 5000)
+            }
+        }
+        // animation.add({
+        //         targets: '.ml5 .line',
+        //         opacity: [0.5,1],
+        //         scaleX: [0, 1],
+        //         easing: "easeInOutExpo",
+        //         duration: 700
+        //     }).add({
+        //         targets: '.ml5 .line',
+        //         duration: 600,
+        //         easing: "easeOutExpo",
+        //         translateY: (el, i) => (-val1 + val2*2*i) + "em"
+        //     }).add({
+        //         targets: '.ml5 .ampersand',
+        //         opacity: [0,1],
+        //         scaleY: [0.5, 1],
+        //         easing: "easeOutExpo",
+        //         duration: 600,
+        //         offset: '-=600'
+        //     }).add({
+        //         targets: '.ml5 .letters-left',
+        //         opacity: [0,1],
+        //         translateX: ["0.5em", 0],
+        //         easing: "easeOutExpo",
+        //         duration: 600,
+        //         offset: '-=300'
+        //     }).add({
+        //         targets: '.ml5 .letters-right',
+        //         opacity: [0,1],
+        //         translateX: ["-0.5em", 0],
+        //         easing: "easeOutExpo",
+        //         duration: 600,
+        //         offset: '-=600'
+        //     }).add({
+        //         targets: '.ml5',
+        //         opacity: 0,
+        //         duration: 1000,
+        //         easing: "easeOutExpo",
+        //         delay: 1000
+        //     });
+        // window.onscroll = animation.play
+    }
 }
 
