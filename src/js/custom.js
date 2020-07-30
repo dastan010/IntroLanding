fab096b26bdf158b1e23f63ebc229ec285875540
Dastan Akhmetov <dastan.mvp97@gmail.com>
2020-07-30 13:46:46 +0600

    almost done with snap effect

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,7 +5,9 @@ window.onclick = () => {
 let screenWidth = window.screen.width,
     screenHeight = window.screen.height,
     val1 = 0,
-    val2 = 0
+    val2 = 0,
+    imageDataArray = [],
+    canvasCount = 35
 
 switch (screenWidth) {
     case 320 :
@@ -34,22 +36,55 @@ function scrollFunc(){
 
         if(makeVisual){
             makeVisual.style.visibility = 'visible'
-            makeVisual.style.setProperty('--animate-duration', '5s');
+            makeVisual.style.setProperty('--animate-duration', '5s')
             makeVisual.classList.add(...mVclasses)
-        }
-        if(window.screen.width > 1200){
-            let visualContainer = document.getElementById('visualContainer'),
-                img = document.createElement("img")
-            img.setAttribute("src","./assets/makeVisual.png")
-            img.setAttribute("class","makeVisualImg")
-            if(visualContainer.childElementCount === 1){
-                setTimeout(()=> {
-                    if(makeVisual) {
+            if(window.screen.width > 1200){
+                let visualContainer = document.getElementById('visualContainer'),
+                    img = document.createElement("img")
+
+
+                img.setAttribute("src","./assets/makeVisual.png")
+                img.setAttribute("class","makeVisualImg")
+                if(visualContainer.childElementCount === 1){
+                    setTimeout(()=> {
                         let oldChild = document.getElementById('makeVisual')
                         visualContainer.removeChild(oldChild)
                         visualContainer.appendChild(img)
-                    }
-                }, 5000)
+
+                        html2canvas(img,{width : 638, height : 100}).then(canvas => {
+                            let ctx = canvas.getContext("2d"),
+                                imageData = ctx.getImageData(0, 0, canvas.width, canvas.height),
+                                pixelArr = imageData.data
+                            createBlankImageData(imageData)
+                            //put pixel info to imageDataArray (Weighted Distributed)
+                            for (let i = 0; i < pixelArr.length; i+=4) {
+                                //find the highest probability canvas the pixel should be in
+                                let p = Math.floor((i/pixelArr.length) * canvasCount)
+                                let a = imageDataArray[weightedRandomDistrib(p)]
+                                a[i] = pixelArr[i]
+                                a[i+1] = pixelArr[i+1]
+                                a[i+2] = pixelArr[i+2]
+                                a[i+3] = pixelArr[i+3]
+                            }
+
+                            //create canvas for each imageData and append to target element
+                            for (let i = 0; i < canvasCount; i++) {
+                                let c = newCanvasFromImageData(imageDataArray[i], canvas.width, canvas.height)
+                                c.classList.add("dust")
+                                $('#visualContainer').append(c)
+                            }
+                            $("#visualContainer").children().not(".dust").fadeOut(3500)
+                            $(".dust").each( function(index){
+                                animateBlur($(this),0.8,800)
+                                setTimeout(() => {
+                                    animateTransform($(this),100,-100,chance.integer({ min: -15, max: 15 }),800+(110*index));
+                                }, 70*index);
+                                //remove the canvas from DOM tree when faded
+                                $(this).delay(70*index).fadeOut((110*index)+800,"easeInQuint",()=> {$( this ).remove();});
+                            });
+                        })
+                    }, 5000)
+                }
             }
         }
         // animation.add({
@@ -93,5 +128,68 @@ function scrollFunc(){
         //     });
         // window.onscroll = animation.play
     }
+}
+
+function weightedRandomDistrib(peak) {
+    let prob = [], seq = [];
+    for(let i=0;i<canvasCount;i++) {
+        prob.push(Math.pow(canvasCount-Math.abs(peak-i),3));
+        seq.push(i);
+    }
+    return chance.weighted(seq, prob);
+}
+
+function createBlankImageData(imageData) {
+    imageData.width = 1200
+    console.log(imageData)
+    for(let i=0;i<canvasCount;i++)
+    {
+        let arr = new Uint8ClampedArray(imageData.data);
+        for (let j = 0; j < arr.length; j++) {
+            arr[j] = 0;
+        }
+        imageDataArray.push(arr);
+    }
+}
+
+function newCanvasFromImageData(imageDataArray ,w , h) {
+    let canvas = document.createElement('canvas')
+    canvas.width = w
+    canvas.height = h
+    tempCtx = canvas.getContext("2d")
+    tempCtx.putImageData(new ImageData(imageDataArray, w , h), 0, 0)
+    return canvas;
+}
+
+function animateTransform(elem,sx,sy,angle,duration) {
+    let td = tx = ty =0
+    $({x: 0, y:0, deg:0}).animate({x: sx, y:sy, deg:angle}, {
+        duration: duration,
+        easing: "easeInQuad",
+        step: function(now, fx) {
+            if (fx.prop == "x")
+                tx = now
+            else if (fx.prop == "y")
+                ty = now
+            else if (fx.prop == "deg")
+                td = now
+            elem.css({
+                transform: 'rotate(' + td + 'deg)' + 'translate(' + tx + 'px,'+ ty +'px)'
+            });
+        }
+    });
+}
+
+function animateBlur(elem,radius,duration) {
+    let r =0;
+    $({rad:0}).animate({rad:radius}, {
+        duration: duration,
+        easing: "easeOutQuad",
+        step: function(now) {
+            elem.css({
+                filter: 'blur(' + now + 'px)'
+            });
+        }
+    });
 }
 
